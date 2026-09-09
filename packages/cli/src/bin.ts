5408eaaf19988a8c79c080c4e847e6bfe6f1b2f4
AyushDubey23 <ayushdubey3740@gmail.com>
2026-09-09 21:45:59 +0530

    chore(cli): release v0.2.0 with actionable remediation guidance

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -23,7 +23,7 @@ const program = new Command()
 program
   .name('flakerad')
   .description('A controlled diagnostic instrument for flaky tests by Ayush Dubey')
-  .version('0.1.0')
+  .version('0.2.0')
 
 program
   .command('init')
